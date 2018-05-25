c2715aa5e51c9cbff1085b4e3672de03ecdebd10
teddy <aismailov@ya.ru>
2018-05-25 23:17:13 +0500

    Update payment process to lock records with save_for_update(), small fixed

--- payments/serializers.py
+++ payments/serializers.py
@@ -32,11 +32,12 @@ class PaymentSerializer(serializers.Serializer):
             to_account = Account.objects.get(pk=to_account)
         except Account.DoesNotExist:
             raise serializers.ValidationError("to_account does not exist")
+
         if from_account.currency != to_account.currency:
             raise serializers.ValidationError("Accounts currencies not equal")
 
-        data["from_account"] = from_account
-        data["to_account"] = to_account
+        # data["from_account"] = from_account
+        # data["to_account"] = to_account
 
         return data
 
@@ -45,6 +46,9 @@ class PaymentSerializer(serializers.Serializer):
         from_account = validated_data["from_account"]
         to_account = validated_data["to_account"]
         amount = validated_data["amount"]
+
+        from_account = Account.objects.select_for_update().get(pk=from_account)
+        to_account = Account.objects.select_for_update().get(pk=to_account)
 
         from_account.balance = from_account.balance - amount
         to_account.balance = to_account.balance + amount
